cc2261e9a5bdee99449b639da12e5730016b2a2f
Alexandre HUBERT <alexandre_hubert@hotmail.fr>
2020-03-25 22:32:50 +0100

    Update the test according to the IMG (detection) updates)

--- src/test_cmn-lib_img_detection.cpp
+++ src/test_cmn-lib_img_detection.cpp
@@ -9,6 +9,7 @@
 
 
 #include <iostream>
+/*
 #include <opencv2/opencv.hpp>
 #include <opencv2/opencv_modules.hpp>
 #include <opencv2/calib3d.hpp>
@@ -26,73 +27,46 @@
 #include <opencv2/stitching.hpp>
 #include <opencv2/video.hpp>
 #include <opencv2/videoio.hpp>
+*/
+#include <IMG_Detection.hpp>
 
 
 
 using namespace std;
 using namespace cv;
+using namespace IMG;
 
 
 
 int main( int argc, char** argv )
 {
-	VideoCapture cap("/home/ahu/Workspace/py-video-processing-rsc/video_data/finale_match_1.mp4");
-	if(!cap.isOpened())
-	{
-		cout << "Cannot open the web cam" << endl;
-		return -1;
-	}
+	cout << "Test C++ library for img detection" << endl;
 
-	while(true)
-	{
-		// Read a new frame from video
-		Mat imgOriginal;
-		bool bSuccess = cap.read(imgOriginal);
-		if(!bSuccess)
+	VideoCapture	l_videoCapture;
+	Mat 			l_inImgCamera;
+	CDetection 		l_detection;
+
+	bool l_readVideoSuccess;
+
+	// Open the video
+		l_videoCapture.open("/home/ahu/Workspace/py-video-processing-rsc/video_data/finale_match_1.mp4");
+		if(!l_videoCapture.isOpened())
 		{
-			cout << "Cannot read a frame from video stream" << endl;
-			break;
+			cout << "Cannot open the the video" << endl;
+			return 0;
 		}
 
-		// Convert the captured frame from BGR to HSV
-		Mat imgHSV;
-		cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV);
-
-		// Threshold the image
-		Mat imgThresholdedGreen;
-		Mat imgThresholdedRedA;
-		Mat imgThresholdedRedB;
-		//inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded);
-		inRange(imgHSV, Scalar(30, 75, 0),		Scalar(70, 255, 255),	imgThresholdedGreen);
-		inRange(imgHSV, Scalar(0, 100, 100),	Scalar(10, 255, 255),	imgThresholdedRedA);
-		inRange(imgHSV, Scalar(160, 100, 100),	Scalar(180, 255, 255),	imgThresholdedRedB);
-
-		// Generate the final mask
-		Mat imgMask;
-		Mat imgThresholded;
-		imgMask			= imgThresholdedGreen + imgThresholdedRedA + imgThresholdedRedB;
-		imgThresholded 	= imgThresholdedGreen + imgThresholdedRedA + imgThresholdedRedB;
-
-		// Morphological opening (remove small objects from the foreground)
-		erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-		dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-
-		// Morphological closing (fill small holes in the foreground)
-		dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-		erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-
-		// Show the original and the thresholded image
-		imshow("Mask Image", 		imgMask);
-		imshow("Thresholded Image",	imgThresholded);
-		imshow("Original Image", 	imgOriginal);
-
-		// Wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
-		if(waitKey(30) == 27)
+	// Read a video frame and convert it from BGR to HSV
+		do
 		{
-			cout << "esc key is pressed by user" << endl;
-			break;
+			l_readVideoSuccess = l_videoCapture.read(l_inImgCamera);
+
+			if(l_readVideoSuccess == true)
+			{
+				l_detection.cupsDetection(l_inImgCamera);
+			}
 		}
-	}
+		while(l_readVideoSuccess == true);
 
 	return 0;
 }
